df7925fc4d3f851ae519a6256d214e24b1987a28
ChrisArcand <chris@chrisarcand.com>
2014-02-12 09:30:29 -0600

    Moar tests

--- spec/opsicle/ssh_spec.rb
+++ spec/opsicle/ssh_spec.rb
@@ -27,6 +27,26 @@ module Opsicle
         subject.execute
       end
 
+      it "should execute ssh with public_ip listings as well as elastic_ip" do
+        subject.stub(:instances) {[
+                                    { hostname: "host1", elastic_ip: "678.678.678.678" },
+                                    { hostname: "host2", public_ip: "987.987.987.987" }
+                                  ]}
+
+        subject.should_receive(:system).with("ssh mrderpyman2014@987.987.987.987")
+        subject.execute
+      end
+
+      it "should execute ssh favoring an elastic_ip over a public_ip if both exist" do
+        subject.stub(:instances) {[
+                                    { hostname: "host1", elastic_ip: "678.678.678.678" },
+                                    { hostname: "host2", public_ip: "987.987.987.987", elastic_ip: "132.132.132.132" }
+                                  ]}
+
+        subject.should_receive(:system).with("ssh mrderpyman2014@132.132.132.132")
+        subject.execute
+      end
+
       it "should execute ssh right away if there is only one Opsworks instance available" do
         subject.stub(:instances) {[
                                     { hostname: "host3", elastic_ip: "456.456.456.456" }
